331af77c954d966e0512396916d2f4da4832a702
M307219 <fernando.p.coelho@bradesco.com.br>
2022-10-24 08:47:30 -0300

    Inclusão corrida

---
+++ Corrida/corrida.py
@@ -0,0 +1,29 @@
+#Informar a distância e tempo da corrida
+distancia = input('Informe a distância percorrida: ')
+tempo = input('Informe o tempo, formato HH:MM:SS : ')
+
+#calculo do pace
+from datetime import datetime
+minutos = datetime.strptime(tempo, "%H:%M:%S").minute
+hora = (datetime.strptime(tempo, "%H:%M:%S").hour)*60
+segundos = (datetime.strptime(tempo, "%H:%M:%S").second)/60
+var1 = (minutos + hora + segundos)/float(distancia)
+num = float((var1 - int(var1))*60)/100
+
+pace = int(var1) + num
+
+#velocidade em km/h
+velocidade_media = 60/pace
+
+#velocidade em m/s
+veloc_ms = velocidade_media / 3.6
+
+#alterando a formatação das casas decimais
+n_formatado = "{:.2f}".format(pace)
+v_formatado = "{:.2f}".format(velocidade_media)
+ms_formatado = "{:.2f}".format(veloc_ms)
+
+#resultados
+print('O pace da corrida é: ' + str(n_formatado)+ ' min/km')
+print('A velocidade média é: ' + str(v_formatado) + ' km/h')
+print('A velocidade média é: ' + str(ms_formatado) + ' m/s')
